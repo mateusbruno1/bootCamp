e44f96421b09683f9bdb0b8b1d7d5837a6527d08
mateusbruno1 <mateusbruno294@gmail.com>
2020-03-16 20:57:42 -0300

    validações

---
+++ modulo02/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd7fa91b3a4dfca21cfc3e979906af77a',
+  expiresIn:'7d',
+}
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,11 +1,16 @@
 import  {Router} from  'express';
-import User from './app/models/User';
+
 
 import UserController from './app/controllers/UserController';
-
+import sessionController from './app/controllers/SessionController';
+import authMiddleaware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/users',UserController.store);
+routes.post('/sessions',sessionController.store);
+routes.use(authMiddleaware)
+routes.put('/users',UserController.update);
+
 
 
 export default  routes;
