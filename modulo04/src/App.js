259e8dd64525d01dac4a47b336e8b2e2067b3f1a
mateusbruno1 <mateusbruno294@gmail.com>
2020-04-12 12:46:04 -0300

    introdução ao react

---
+++ modulo04/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TechList from './Components/TechList'
+import './App.css'
+
+function App() {
+  return <TechList />
+}
+export default App;
---
+++ modulo04/src/Components/TechList.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import TechItem from './TechItem'
+
+class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: []
+  };
+
+  // Executa assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  // _ = prevProps
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {
+
+  }
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault(); // para não executar o load da page
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '', // para limpar o input
+    });
+  }
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+
+      </form>
+    );
+  }
+}
+
+export default TechList;
